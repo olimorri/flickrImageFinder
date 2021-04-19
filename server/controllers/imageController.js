c40ac4d77132cdee8c05978a2e93513d1ce741ee
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-04-19 08:37:00 +0100

    feat: updated controller to include error handling:

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -1,13 +1,15 @@
 const axios = require('axios')
 
 //Get Photos
-
 async function getPhotos (req,res) {
-  //TODO: this needs to tidied up, to properly handle errors etc.
-  const photos = await axios.get(`https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=true&lang=en-us`);
-  res.send(photos.data.items);
-
- 
+  try {
+    const photos = await axios.get(`https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=true&lang=en-us`);
+    res.status(200);
+    res.send(photos.data.items);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 }
 
 module.exports = {getPhotos}
